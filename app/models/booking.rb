a9994ca9acbd810a163c334096dafb0a7e636734
andre ferrer <andrerferrer@hotmail.com>
2021-06-07 09:48:14 -0300

    add scope for past and future bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :restaurant
+
+  scope :past, -> { where('DATE(booked_on) < ?', Date.today) }
+  scope :future, -> { where('DATE(booked_on) > ?', Date.today) }
 end

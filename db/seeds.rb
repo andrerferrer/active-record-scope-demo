916e075bbf3f2d125f018cd9b6e0839412b2abee
andre ferrer <andrerferrer@hotmail.com>
2021-06-07 09:44:17 -0300

    add some booking seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,16 @@ Restaurant.all.each do |restaurant|
   meals.each do |meal|
     Meal.create! **meal, restaurant: restaurant
   end
+
+  Booking.create!(
+    restaurant: restaurant,
+    booked_on: 1.day.ago
+  )
+
+  Booking.create!(
+    restaurant: restaurant,
+    booked_on: 1.day.from_now
+  )
 end
 
 
